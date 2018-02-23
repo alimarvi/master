626e5d413b0f79dd5179918f6779ef0d3fb6da2b
Marvi <MARVIA2@TDBFG.COM>
2018-02-22 20:28:42 -0500

    added all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { Test01Component } from './test01/test01.component';
+import { Test02Component } from './test02/test02.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataService } from './appServices';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Test01Component,
+    Test02Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserModule.withServerTransition({ appId: 'td-frontend-dev-test-app' }),
+    HttpClientModule,
+    BrowserAnimationsModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [DataService, { provide: LOCALE_ID, useValue: 'fr' }]
 })
 export class AppModule { }
---
+++ src/app/test01/test01.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Test01Component } from './test01.component';
+import { Data, DataService } from '../appServices';
+
+
+describe('Test01Component', () => {
+  let component: Test01Component;
+  let fixture: ComponentFixture<Test01Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Test01Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Test01Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
